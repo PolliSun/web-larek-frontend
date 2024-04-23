4ad96e8db3753938f40438da6cdbf754e92e8d9b
Polina Lepekhina <wdvrgnyjio6@gmail.com>
2024-04-24 01:08:58 +0300

    redmi file typing is configured

---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+//Данные карточки
+export interface ICard {
+	id: string;
+	category?: string;
+	title: string;
+	description?: string;
+	image?: string;
+	price: number | null;
+}
+
+//Данные формы оплаты
+export interface IOrderForm {
+	address: string;
+	email: string;
+	phone: string;
+	payment: PaymentMethod;
+}
+
+// Список для заказа
+export interface IOrder  extends IOrderForm {
+    total: number;
+    items: string[];
+}
+
+//Заполнить каталог
+export interface IOrderResult {
+    id: string;
+    total: number;
+}
+
+//Вид оплаты
+export type PaymentMethod = "online" | "personally";
+
+//Методы
+export interface IAppState {
+    catalog: ICard[];
+    bascet: ICard[];
+    preview: string | null;
+    order: IOrder | null;
+}
+
+//Ошибки формы
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
